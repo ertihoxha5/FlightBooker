37149af88bcdd727a5ad8de4834bd5bc232a969b
ertihoxha5 <ertihoxha874@gmail.com>
2025-04-11 11:41:54 +0200

    Konfigurimi fillestar i App.js me routimin per  login

---
+++ flightbooker/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login"
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      
+        <Route path="/" element={<Login />} />
+
+        
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
