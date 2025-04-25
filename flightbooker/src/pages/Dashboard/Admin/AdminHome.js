1bf17d9394712399db9db8dcd1de09a07dcffeb3
ertihoxha5 <ertihoxha874@gmail.com>
2025-04-25 19:53:25 +0200

    Krijimi i Home Page per Adminin

---
+++ flightbooker/src/pages/Dashboard/Admin/AdminHome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FaPlane, FaUsers, FaMoneyBill, FaChartBar, FaClipboardList } from 'react-icons/fa';
+
+const AdminHome = () => {
+  return (
+    <div className="container-fluid">
+
+      <h1 className="h3 mb-4 text-gray-800">Welcome Back, Admin</h1>
+
+
+    </div>
+  );
+};
+
+export default AdminHome;
