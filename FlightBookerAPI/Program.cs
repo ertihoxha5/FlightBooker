ad24c83b1908dbe202fbdcacfcc956199f317acd
ertihoxha5 <ertihoxha874@gmail.com>
2025-05-02 13:09:52 +0200

    Konfigurimi i middleware

--- FlightBookerAPI/Program.cs
+++ FlightBookerAPI/Program.cs
@@ -50,3 +50,18 @@ builder.Services.AddSwaggerGen(c =>
 });
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
