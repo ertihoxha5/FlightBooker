e2d579dabda928f4d7999078d21e6a7d22bdc935
ertihoxha5 <ertihoxha874@gmail.com>
2025-05-02 13:21:40 +0200

    Pjesa e ApplicationDbContext

---
+++ FlightBookerAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using FlightBookerAPI.Models;
+
+namespace FlightBookerAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SeedData.Seed(modelBuilder); 
+        }
+    }
+}
