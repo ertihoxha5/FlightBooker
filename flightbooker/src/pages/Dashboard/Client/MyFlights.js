5fb25e7c3c602b0bd20c11f296a1f092d5b932b2
MerisaShallci <shallcimerisa@gmail.com>
2025-05-03 12:54:15 +0200

    vazhdim

--- flightbooker/src/pages/Dashboard/Client/MyFlights.js
+++ flightbooker/src/pages/Dashboard/Client/MyFlights.js
@@ -1,4 +1,6 @@
 
 import React, { useEffect, useState } from 'react';
-
+const MyFlights = () => {
+    const [flights, setFlights] = useState([]);
+    const [loading, setLoading] = useState(true);
 
