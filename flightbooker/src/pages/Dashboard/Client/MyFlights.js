c38431863ba156e62b9928fa1129565844f47c82
MerisaShallci <shallcimerisa@gmail.com>
2025-05-03 12:55:30 +0200

    commit

--- flightbooker/src/pages/Dashboard/Client/MyFlights.js
+++ flightbooker/src/pages/Dashboard/Client/MyFlights.js
@@ -3,4 +3,5 @@ import React, { useEffect, useState } from 'react';
 const MyFlights = () => {
     const [flights, setFlights] = useState([]);
     const [loading, setLoading] = useState(true);
-
+ // Mock data - do zëvendësohet me të dhëna reale nëse përdor API
+ const mockFlights = [
